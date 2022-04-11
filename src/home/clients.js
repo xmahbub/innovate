b61a932fd6ffc3579bcbfb67e60fecd8302e9dcc
Mahbub Hasan <38033407+xmahbub@users.noreply.github.com>
2022-04-11 13:48:38 +0600

    updated
    updated

--- src/home/clients.js
+++ src/home/clients.js
@@ -10,7 +10,7 @@ import AMAZONE from '../images/amazon.svg';
 
 export default function Clients(){
     return(
-        <div className="flex flex-wrap items-center justify-center relative mx-16 ">
+        <div className="flex flex-wrap items-center justify-center relative mx-6 lg:mx-16 md:mx-16">
             
             <div><img title="Instacart" src={BBC} alt="BBC" class="h-5 sm:h-8 mx-3 my-1 sm:mx-3 sm:my-1 md:mx-4 md:my-3 opacity-25"></img></div>
             <div><img title="Instacart" src={NIKE} alt="BBC" class="h-5 sm:h-8 mx-3 my-1 sm:mx-3 sm:my-1 md:mx-4 md:my-3 opacity-25"></img></div>
